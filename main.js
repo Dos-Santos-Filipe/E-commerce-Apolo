02206be21be47ce5f420f59a70f2f4988c9483bb
Filipe Dos Santos <filipesantos.psic@gmail.com>
2023-09-03 11:21:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import { renderizarCatalogo } from "./src/cardProduto";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
+import {
+  atualizarPrecoCarrinho,
+  inicializarCarrinho,
+  renderizarProdutosCarrinho,
+} from "./src/menuCarrinho";
+
+renderizarCatalogo();
+inicializarCarrinho();
+renderizarProdutosCarrinho();
+atualizarPrecoCarrinho();
+inicializarFiltros();
